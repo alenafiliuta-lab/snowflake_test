c03bd414a886ca9c3c6b4928c67a9cd97caf5d66
Alena Filiuta <alena.filiuta@innowise.com>
2026-04-30 09:08:39 +0000

    delete some comments rows

--- E1WQI8V5KON034RN/streamlit_app.py
+++ E1WQI8V5KON034RN/streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 
-# Получаем активную сессию Snowflake (приложение уже авторизовано под APP_CLONER_ROLE)
 session = get_active_session()
 
 st.set_page_config(page_title="Snowflake DB Clone Tool", layout="centered")
 st.title("🏁 Snowflake DB Clone Tool")
 
-# default_target = "DEV_CLONE"
 
 with st.form("clone_form"):
     source_db = st.text_input("Clone from DB (source)", value="MY_DB").upper()
@@ -15,7 +13,6 @@ with st.form("clone_form"):
     owner_role = st.text_input("Owner role (development)", value="DEVELOPER_ROLE").upper()
     readonly_role = st.text_input("Read-only role", value="READONLY_ROLE").upper()
     
-    # Кнопка отправки формы — обязательно!
     submitted = st.form_submit_button("Clone & Setup Permissions")
 if submitted:
     if not source_db or not target_db or not owner_role or not readonly_role:
@@ -24,8 +21,6 @@ if submitted:
 
     try:
         st.info("Cloning database...")
-        
-        # Шаг 1: Клонирование базы данных
         clone_sql = f"""
             CREATE DATABASE IF NOT EXISTS {target_db}
             CLONE {source_db};
@@ -33,7 +28,6 @@ if submitted:
         session.sql(clone_sql).collect()
         st.success(f"✅ Database '{target_db}' cloned from '{source_db}'")
 
-        # Шаг 2: Права для роли разработчика (owner_role)
         st.info("Granting all privileges to Developer role...")
         session.sql(f"GRANT OWNERSHIP ON DATABASE {target_db} TO ROLE {owner_role} COPY CURRENT GRANTS").collect()
         session.sql(f"GRANT ALL PRIVILEGES ON DATABASE {target_db} TO ROLE {owner_role}").collect()
@@ -45,7 +39,7 @@ if submitted:
         session.sql(f"GRANT ALL PRIVILEGES ON FUTURE VIEWS IN DATABASE {target_db} TO ROLE {owner_role}").collect()
         st.success(f"🔑 Full privileges granted to {owner_role}")
 
-        # Шаг 3: Права для роли аналитика (readonly_role)
+    
         st.info("Granting READONLY privileges to Analyst role...")
         session.sql(f"GRANT USAGE ON DATABASE {target_db} TO ROLE {readonly_role}").collect()
         session.sql(f"GRANT USAGE ON ALL SCHEMAS IN DATABASE {target_db} TO ROLE {readonly_role}").collect()
